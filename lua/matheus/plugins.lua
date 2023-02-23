77373a5006be522a28175acae415e24d1831e6ef
matheus-mcosta <matheus.costa@inf.ufrgs.br>
2023-02-23 14:27:58 -0300

    nvim-tree

--- lua/matheus/plugins.lua
+++ lua/matheus/plugins.lua
@@ -1,74 +1,84 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	"nvim-lua/plenary.nvim",
-	-- colorscheme
-	"sainnhe/gruvbox-material",
-	"p00f/nvim-ts-rainbow",
-	"rafamadriz/friendly-snippets",
-	{
-		"nvim-telescope/telescope.nvim",
-		version = "0.1.0",
-		-- or                            , branch = '0.1.x',
-		dependencies = { { "nvim-lua/plenary.nvim" } },
-	},
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},{
-		"ggandor/leap.nvim",
-		config = function()
-		require('leap').add_default_mappings()
-    end,
-	},
+    "nvim-lua/plenary.nvim",
+    -- colorscheme
+    "sainnhe/gruvbox-material",
+    "p00f/nvim-ts-rainbow",
+    "rafamadriz/friendly-snippets",
+    {
+        'nvim-tree/nvim-tree.lua',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        config = function ()
+            require("nvim-tree").setup()
+        end
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        version = "0.1.0",
+        -- or                            , branch = '0.1.x',
+        dependencies = { { "nvim-lua/plenary.nvim" } },
+    },
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup({})
+        end,
+    },
+    {
+        "ggandor/leap.nvim",
+        config = function()
+            require("leap").add_default_mappings()
+        end,
+    },
 
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-	{ "akinsho/bufferline.nvim", version = "v3.*", dependencies = "nvim-tree/nvim-web-devicons" },
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-	},
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
+    },
+    { "akinsho/bufferline.nvim", version = "v3.*",       dependencies = "nvim-tree/nvim-web-devicons" },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+    },
 
-	"norcalli/nvim-colorizer.lua",
+    "norcalli/nvim-colorizer.lua",
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-	},
-	-- LSP
-	{
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-		"jose-elias-alvarez/null-ls.nvim",
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+    -- LSP
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        "jose-elias-alvarez/null-ls.nvim",
+    },
 
-	-- CMP
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/nvim-cmp",
-	"onsails/lspkind-nvim",
-	"nvim-lua/lsp_extensions.nvim",
-	"simrat39/symbols-outline.nvim",
-	"L3MON4D3/LuaSnip",
-	"saadparwaiz1/cmp_luasnip",
-	{ "tzachar/cmp-tabnine", build = "./install.sh", dependencies = "hrsh7th/nvim-cmp" },
+    -- CMP
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/nvim-cmp",
+    "onsails/lspkind-nvim",
+    "nvim-lua/lsp_extensions.nvim",
+    "simrat39/symbols-outline.nvim",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    { "tzachar/cmp-tabnine",     build = "./install.sh", dependencies = "hrsh7th/nvim-cmp" },
 })
--- lua/matheus/remap.lua
+++ lua/matheus/remap.lua
@@ -1,7 +1,7 @@
 local nnoremap = require("matheus.keymap").nnoremap
 local inoremap = require("matheus.keymap").inoremap
 
-nnoremap("<leader>pv","<cmd>Ex<CR>")
+nnoremap("<leader>pv","<cmd>:NvimTreeToggle<CR>")
 nnoremap("<leader>w","<cmd>:w<CR>")
 nnoremap("<leader>ff","<cmd>lua require('telescope.builtin').find_files()<CR>")
 nnoremap("<leader>fg","<cmd>lua require('telescope.builtin').live_grep()<CR>")
