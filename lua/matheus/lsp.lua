1ec91335fdbf0ff9ae57e9baa5d834e834f61a49
Matheus Costa <matheus.costa@inf.ufrgs.br>
2023-04-16 15:44:33 -0300

    Disable pyright type check

--- lua/matheus/lsp.lua
+++ lua/matheus/lsp.lua
@@ -1,74 +1,84 @@
 local nnoremap = require("matheus.keymap").nnoremap
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "pyright",
-        "gopls",
-        "clangd",
-        "rust_analyzer",
-        "tsserver",
-        "eslint",
-        "html",
-        "cssls",
-        "tailwindcss",
-    },
-    automatic_installation = true,
+	ensure_installed = {
+		"pyright",
+		"gopls",
+		"clangd",
+		"rust_analyzer",
+		"tsserver",
+		"eslint",
+		"html",
+		"cssls",
+		"tailwindcss",
+	},
+	automatic_installation = true,
 })
 
 -- Mappings
 local on_attach = function()
-    nnoremap("T", vim.lsp.buf.hover, { buffer = 0 })
-    nnoremap("<leader>lr", vim.lsp.buf.rename)
-    nnoremap("<leader>la", vim.lsp.buf.code_action)
-    nnoremap("<leader>gd", vim.lsp.buf.definition)
-    nnoremap("<leader>gD", vim.lsp.buf.declaration)
-    nnoremap("<leader>gi", vim.lsp.buf.implementation)
-    nnoremap("<leader>gr", require("telescope.builtin").lsp_references)
-    nnoremap("<leader>lf", function()
-        vim.lsp.buf.format({ async = true })
-    end)
+	nnoremap("T", vim.lsp.buf.hover, { buffer = 0 })
+	nnoremap("<leader>lr", vim.lsp.buf.rename)
+	nnoremap("<leader>la", vim.lsp.buf.code_action)
+	nnoremap("<leader>gd", vim.lsp.buf.definition)
+	nnoremap("<leader>gD", vim.lsp.buf.declaration)
+	nnoremap("<leader>gi", vim.lsp.buf.implementation)
+	nnoremap("<leader>gr", require("telescope.builtin").lsp_references)
+	nnoremap("<leader>lf", function()
+		vim.lsp.buf.format({ async = true })
+	end)
 end
 
 nnoremap("<leader>lf", function()
-    vim.lsp.buf.format({ async = true })
+	vim.lsp.buf.format({ async = true })
 end)
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-
 -- Automatic lsp setup
 
 require("mason-lspconfig").setup_handlers({
-    function(server)
-        require("lspconfig")[server].setup({ on_attach = on_attach, capabilities = capabilities })
-    end,
+	function(server)
+		require("lspconfig")[server].setup({ on_attach = on_attach, capabilities = capabilities })
+	end,
 })
 
+require("lspconfig")["pyright"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "off",
+			},
+		},
+	},
+})
 
 local null_ls = require("null-ls")
 require("null-ls").setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.autopep8,
-        null_ls.builtins.formatting.sql_formatter,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.code_actions.eslint,
-    },
-    on_attach = on_attach,
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.autopep8,
+		null_ls.builtins.formatting.sql_formatter,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.code_actions.eslint,
+	},
+	on_attach = on_attach,
 })
 
 require("nvim-treesitter.configs").setup({
-    highlight = { enable = true },
-    auto_install = true,
-    -- ...
-    -- ...
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-    },
+	highlight = { enable = true },
+	auto_install = true,
+	-- ...
+	-- ...
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
 })
