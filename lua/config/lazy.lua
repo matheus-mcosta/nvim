927fc821299ede1ac476c9fa010bef1e1aff6e5c
Matheus Costa <matheus.costa@inf.ufrgs.br>
2023-09-26 18:24:57 -0300

    Add more languages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
 
     { import = "plugins" },
     { import = "lazyvim.plugins.extras.dap.core" },
