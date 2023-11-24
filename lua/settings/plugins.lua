2f46a8bb17fa7c61eeaf6103b4250d796796c5d7
matheus-mcosta <matheus.costa@inf.ufrgs.br>
2023-11-24 01:36:49 -0300

    Add surround

--- lua/settings/plugins.lua
+++ lua/settings/plugins.lua
@@ -28,7 +28,16 @@ require("lazy").setup({
     end,
   },
   { "akinsho/bufferline.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
-
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
