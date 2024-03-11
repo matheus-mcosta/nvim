2f2d7ad3cffe42218e1bd57b8fd301f375cf3f6c
matheus-mcosta <matheus.costa@inf.ufrgs.br>
2024-03-11 16:43:30 -0300

    Add treesitter-context and golangci linter

--- lua/settings/lsp.lua
+++ lua/settings/lsp.lua
@@ -48,6 +48,7 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.golangci_lint,
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.code_actions.gomodifytags,
         null_ls.builtins.code_actions.impl
