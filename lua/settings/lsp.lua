a91e3647936c5fd6e20630dd10848a963f79eff6
matheus-mcosta <matheus.costa@inf.ufrgs.br>
2024-08-23 14:20:25 -0300

    feat: add isort

--- lua/settings/lsp.lua
+++ lua/settings/lsp.lua
@@ -49,6 +49,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
 		null_ls.builtins.formatting.rubocop,
 		null_ls.builtins.formatting.sql_formatter,
 		null_ls.builtins.diagnostics.golangci_lint,
